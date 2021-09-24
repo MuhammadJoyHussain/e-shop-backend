64dce609fa39fedb3165210cf2505c2bfcffd2a0
unknown <muhammadjoyhussain@gmail.comdtrujhtyjnjjtdjhnghj>
2021-09-24 21:33:39 +0600

    preparing backend

---
+++ routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import asynctHandler from 'express-async-handler'
+const router = express.Router()
+import Product from './../models/ptoductModel.js'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+
+router.get('/',
+    asynctHandler(async (req, res) => {
+        const products = await Product.find({})
+
+        res.json(products)
+    })
+)
+
+// @desc    Fetch single products
+// @route   GET /api/products
+// @access  Public
+
+router.get('/:id',
+    asynctHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id)
+
+        if (product) {
+            res.json(product)
+        } else {
+            res.status(404)
+            throw new Error('Product not found')
+        }
+        res.json(product)
+    })
+)
+
+export default router
