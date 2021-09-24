c1fc8431305c064b9a6a7b26206b484c9b14dd6a
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-09-24 21:44:11 +0600

    [ADD]

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -27,7 +27,7 @@ router.get('/:id',
             res.json(product)
         } else {
             res.status(404)
-            throw new Error('Product not found')
+            throw new Error('Product Not Found')
         }
         res.json(product)
     })
