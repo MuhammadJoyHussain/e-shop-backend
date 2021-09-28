d2365410649c4d50ac84731b872a60f009444eb1
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-09-28 17:17:35 +0600

    backend complete

--- data/users.js
+++ data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs'
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@gmail.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },
-    {
-        name: 'Joynul',
-        email: 'joynul@gmail.com',
-        password: bcrypt.hashSync('123456', 10)
-    },
-    {
-        name: 'Muhammad',
-        email: 'muhammad@gmail.com',
-        password: bcrypt.hashSync('123456', 10)
-    },
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
 export default users
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,23 @@
 import express from 'express'
-import { deleteProduct, getProductById, getProducts, createProduct, updateProduct, createProductReview } from '../controller/productController.js'
 const router = express.Router()
-import { protect, admin } from '../middlewere/authMiddlewere.js'
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
